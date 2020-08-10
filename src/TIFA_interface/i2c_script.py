5235eddfb4ebef2b60c763b7d1b7314ad103b425
grad20 <elfeky.ahmedadel@gmail.com>
2020-08-10 10:56:37 +0200

    .....

---
+++ src/TIFA_interface/i2c_script.py
@@ -0,0 +1,32 @@
+import smbus
+import time
+# Nvidia Jetson Nano i2c Bus 0
+bus = smbus.SMBus(0)
+
+# This is the address we setup in the Tiva C Program
+address = 0x0f
+
+def writeNumber(value):
+    bus.write_byte(address, value)
+    # bus.write_byte_data(address, 0, value)
+    return -1
+
+def readNumber(i):
+    m = 0
+    while m<i:
+        number = bus.read_byte(address)
+        print(number)
+        m=m+1
+    # number = bus.read_byte_data(address, 1)
+    return number
+
+while True:
+    var = input("")
+    if not var:
+        continue
+    writeNumber(var)
+    if var ==0x50:
+        number = readNumber(8)
+    if var == 0x43:
+        number = readNumber(1)
+   
---
+++ src/bump_detection_dl_cam_ros.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import helper_functions as hf
+import cv2
+import tensorflow.contrib.tensorrt as trt
+import tensorflow as tf
+import numpy as np
+import os 
+
+import rospy
+from std_msgs.msg import String
+
+
+
+while True :
+
+    retval, image = cap.read()
+    left_right_image = np.split(image, 2, axis=1)
+    image = left_right_image[1]
+    
+    # Detect Here...
+    scores, boxes, classes, num_detections = tf_sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections],
+                                                         feed_dict={tf_input: image[None, ...]})
+    boxes = boxes[0] # index by 0 to remove batch dimension
+    scores = scores[0]
+    classes = classes[0]
+    num_detections = int(num_detections[0])
+
+    # GPIO Signal........................
+    if scores[0] > 0.3:
+        print('Bump :(')
+#         hf.visualize_detection(image, num_detections, classes, boxes, scores)
+    else:
+        print('No Bump :)')
+    #....................................
+    
+    cv2.imshow("Detection", image)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('bump_detection_DL_camera', anonymous=True)
+        rate = rospy.Rate(3)
+        bumpNotficationPub = rospy.Publisher("/bump_detect_ML", String, queue_size=1)
+        # In[2]:
+        current_dir=os.getcwd().split('/')
+        pb_fname="/home/"+current_dir[2]+"/catkin_ws/src/gradsim/src/bump_detect_Camera/ssd_inception_v2_coco_trt.pb"
+        # In[4]:
+        trt_graph = hf.get_frozen_graph(pb_fname)
+        input_names = ['image_tensor']
+        tf_config = tf.ConfigProto()
+        tf_config.gpu_options.allow_growth = True
+        tf_sess = tf.Session(config=tf_config)
+        tf.import_graph_def(trt_graph, name='')
+        tf_input = tf_sess.graph.get_tensor_by_name(input_names[0] + ':0')
+        tf_scores = tf_sess.graph.get_tensor_by_name('detection_scores:0')
+        tf_boxes = tf_sess.graph.get_tensor_by_name('detection_boxes:0')
+        tf_classes = tf_sess.graph.get_tensor_by_name('detection_classes:0')
+        tf_num_detections = tf_sess.graph.get_tensor_by_name('num_detections:0')
+        tf_input.shape.as_list()
+        # In[7]:
+        cap = cv2.VideoCapture("/home/grad20/Desktop/VID_20200807_190001.mp4")
+        width, height, fps = 2560, 720, 25   #cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        while not rospy.is_shutdown():
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
